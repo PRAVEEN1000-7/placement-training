de877053e975339251fd22429944134a692c7d24
PRAVEEN S <151344150+PRAVEEN1000-7@users.noreply.github.com>
2024-07-26 19:15:19 +0530

    Update if_else.py

--- 26.07.2024/if_else.py
+++ 26.07.2024/if_else.py
@@ -1,14 +1,5 @@
 
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
+n = int(input().strip())
 if n%2==1 :
     print("Weird")
 elif n%2==0 and n>=2 and n<=5 :
