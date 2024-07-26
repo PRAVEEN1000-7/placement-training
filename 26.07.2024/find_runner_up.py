726c2c69e93f93083e31e273dd28f2c9feb128c9
PRAVEEN S <151344150+PRAVEEN1000-7@users.noreply.github.com>
2024-07-26 19:13:50 +0530

    Update find_runner_up.py

--- 26.07.2024/find_runner_up.py
+++ 26.07.2024/find_runner_up.py
@@ -1,8 +1,7 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-    x=max(arr)
-    while x in arr:
-        arr.remove(x)
-    y=max(arr)
-    print(y)
+n = int(input())
+arr = list(map(int, input().split()))
+x=max(arr)
+while x in arr:
+    arr.remove(x)
+y=max(arr)
+print(y)
