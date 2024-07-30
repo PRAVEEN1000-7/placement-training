533dfeff229ca22e2d55711c4fb2cbae6093e983
PRAVEEN S <151344150+PRAVEEN1000-7@users.noreply.github.com>
2024-07-30 20:26:54 +0530

    Create pyramid_pattern.py

---
+++ 30.07.2024/pyramid_pattern.py
@@ -0,0 +1,13 @@
+def pyramid(x):
+    
+    for i in range(1,x+1):
+        for j in range(0,x-i+1):
+            print(" ",end="")
+        y=1
+        for j in range(1,i+1):
+            print(" *",sep="",end="")
+
+        print()
+
+x=int(input("Enter the number :"))
+pyramid(x)
